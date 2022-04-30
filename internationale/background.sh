2855e345e69a2917257a7b61317015ba3cffddec
Job Bautista <jobbautista9@protonmail.com>
2022-05-01 01:00:29 +0800

    Restructuring arrange of The Internationale

---
+++ internationale/background.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+DIR="background"
+(echo "\$qu:0";
+ cat $DIR/verse1 \
+     $DIR/verse2 \
+     $DIR/verse1 \
+     $DIR/verse3 ;
+ echo "\$qu:0";
+ cat $DIR/dasg $DIR/dasg $DIR/dasg \
+     $DIR/dasg $DIR/dasf $DIR/verse5 \
+     $DIR/verse6 \
+     $DIR/verse7 \
+     $DIR/chorus/1 $DIR/chorus/2 \
+) | ../stdin/samples.pl 100 4 2 ../piano/ff
---
+++ internationale/foreground.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+DIR="foreground"
+cat $DIR/verse1 \
+    $DIR/verse2 \
+    $DIR/verse1 \
+    $DIR/verse3 \
+    $DIR/verse4 \
+    $DIR/dsdsdds $DIR/verse5 \
+    $DIR/verse6/1 $DIR/dsdsdds $DIR/verse6/2 \
+    $DIR/verse7 \
+    $DIR/chorus/1 $DIR/chorus/2 |
+../stdin/samples.pl 100 4 2 ../piano/ff
