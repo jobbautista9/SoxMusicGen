e139f94a4adf0ec456c88701a5dfbdd4b9196f3a
Job Bautista <jobbautista9@protonmail.com>
2022-05-01 22:27:38 +0800

    Change pierrot arrange shell script to match with Internationale's

--- pierrot-of-the-star-spangled-banner/play.sh
+++ pierrot-of-the-star-spangled-banner/play.sh
@@ -1,18 +1,17 @@
-PIERROT="pierrot-of-the-star-spangled-banner/"
-cat $PIERROT/00 \
-    $PIERROT/01 \
-    $PIERROT/02 \
-    $PIERROT/03/gg \
-    $PIERROT/03/gg \
-    $PIERROT/03/gg \
-    $PIERROT/03/aa \
-    $PIERROT/03/ga \
-    $PIERROT/03/ga \
-    $PIERROT/03/ga \
-    $PIERROT/03/therest \
-    $PIERROT/04-05-common \
-    $PIERROT/04 \
-    $PIERROT/04-05-common \
-    $PIERROT/05 |
-../stdin/synths.pl 180 6 square square triangle square square sawtooth |
-xargs play
+#!/bin/sh
+cat 00 \
+    01 \
+    02 \
+    03/gg \
+    03/gg \
+    03/gg \
+    03/aa \
+    03/ga \
+    03/ga \
+    03/ga \
+    03/therest \
+    04-05-common \
+    04 \
+    04-05-common \
+    05 |
+../stdin/synths.pl 180 6 square square triangle square square sawtooth
